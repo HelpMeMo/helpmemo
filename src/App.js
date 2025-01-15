501b7e02c7aea497f900cb6a2d03c61af747cc23
isabellybarbosac <isabellybcordeiro@gmail.com>
2025-01-14 21:59:50 -0300

    direcionando cards de sociologia para a página correta

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import './App.css';
-import Card from './components/Card';
-import EmileDurkhein from "./assets/EmileDurkheim.png";
-import questio_mark_sociologia from "./assets/questio_mark_sociologia.png";
+
 
 import Header from './components/Header'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -23,10 +21,6 @@ function App() {
           <Route path="/math" element={<Math />} />
           <Route path="/sociology" element={<Sociology />} />
         </Routes>
-        <h1 className="text-3xl w-64 bg-slate-400 text-center">Testes</h1>
-        <Card content={<img className="Questio_mark_sociologia" src={questio_mark_sociologia} alt="question mark sociologia" />} />
-        <Card content={<img className="Questio_mark_sociologia" src={EmileDurkhein} alt="EmileDurkhein" />} />
-
 
 
       </div>
--- src/pages/Sociology.jsx
+++ src/pages/Sociology.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import Card from '../components/Card';
+import EmileDurkhein from "../assets/EmileDurkheim.png";
+import questio_mark_sociologia from "../assets/questio_mark_sociologia.png";
 
 const Sociology = () => {
         return (
-                <div>
+                <><div>
                         <h1>Sociologia</h1>
                         <p>Esta é a página de sociologia.</p>
-                </div>
+                </div><h1 className="text-3xl w-64 bg-slate-400 text-center">Testes</h1><Card content={<img className="Questio_mark_sociologia" src={questio_mark_sociologia} alt="question mark sociologia" />} /><Card content={<img className="Questio_mark_sociologia" src={EmileDurkhein} alt="EmileDurkhein" />} /></>
+
+
         );
 };
 
