fde43d30bd0e1694446670f39007ffe26d0764f6
isabellybarbosac <isabellybcordeiro@gmail.com>
2025-01-14 22:21:53 -0300

    atualização do header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,22 +3,26 @@ import logo from '../assets/logo.png';
 import '../styles/Header.css';
 import { Link } from "react-router-dom";
 
-
-function Navbar() {
+const Navbar = () => {
         return (
                 <nav className="navbar">
-
                         <div className="logo">
                                 <img src={logo} alt="Logo" className="logo-img" />
                         </div>
 
-                        <div className="dropdown">
-                                <button className="dropbtn">Disciplinas</button>
-                                <div className="dropdown-content">
-                                        <Link to="/math">Matemática</Link>
-                                        <Link to="/sociology">Sociologia</Link>
-                                        <Link to="/hst">HST</Link>
+                        <div className="nav-links">
+                                <Link to="/">Início</Link>
+
+                                <div className="dropdown">
+                                        <button className="dropbtn">Disciplinas</button>
+                                        <div className="dropdown-content">
+                                                <Link to="/math">Matemática</Link>
+                                                <Link to="/sociology">Sociologia</Link>
+                                                <Link to="/hst">HST</Link>
+                                        </div>
                                 </div>
+
+                                <Link to="/about">Sobre</Link>
                         </div>
                 </nav>
         );
