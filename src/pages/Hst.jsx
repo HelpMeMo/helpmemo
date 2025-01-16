5a3f381da1fd0be1f39ee7ae0c2691eed0d51fe9
Sophia Moura <araujomsophia@gmail.com>
2025-01-15 23:43:27 -0300

    alter. hst e soc

--- src/pages/Hst.jsx
+++ src/pages/Hst.jsx
@@ -1,12 +1,49 @@
 import React from "react";
 import '../styles/Hst.css';
 
+import Card from '../components/Card';
+import capitalFinanceiro from "../assets/sociology/capitalFincanceiro.png";
+import capitalFinanceiro2 from "../assets/sociology/capitalFinanceiro2.png";
+import capitalCultural from "../assets/sociology/capitalCultural.png";
+import capitalSimbolico from "../assets/sociology/capitalSimbolico.png";
+import karl from "../assets/sociology/karl.png";
+
+import vaca from '../assets/vaca.png'
+import vacaLeite from '../assets/vacaLeite.png'
+
 const Hst = () => {
     return (
+
         <div className="hst-container">
-            <h2 className="titulo">Jogo da Memória</h2>
-            <h1>HIGIENE E SEGURANÇA DO TRABALHO</h1>
-            <p>Esta é a página de HST aaaaaaaaaa</p>
+            <h1 className="hst-firstTitle">JOGO DA MEMÓRIA</h1>
+            
+            <header className="hst-header">
+                Higiene e Segurança do Trabalho (HST)
+            </header>
+            
+            <div className="grid grid-cols-4 gap-4 p-4">
+                <Card content={capitalFinanceiro} />
+                <Card content={capitalFinanceiro2} />
+                <Card content={capitalCultural} />
+                <Card content={capitalSimbolico} />
+                <Card content={karl} />
+                <Card content={capitalFinanceiro} />
+                <Card content={capitalFinanceiro} />
+                <Card content={capitalFinanceiro} />
+            </div>
+            
+            <div className="hst-buttonNext">
+
+                <button id="shuffle-button">Embaralhe para um novo jogo!</button>
+           
+            </div>
+
+            <div>
+
+                <img src={vaca} alt="vaquinha" className="vaca" />
+                <img src={vacaLeite} alt="vaquinha com leite" className="vaca" />
+            
+            </div>
         </div>
     );
 };
--- src/pages/Sociology.jsx
+++ src/pages/Sociology.jsx
@@ -1,34 +1,53 @@
 import React from "react";
 import Card from '../components/Card';
+import '../styles/Sociology.css';
+
 import capitalFinanceiro from "../assets/sociology/capitalFincanceiro.png";
 import capitalFinanceiro2 from "../assets/sociology/capitalFinanceiro2.png";
 import capitalCultural from "../assets/sociology/capitalCultural.png";
 import capitalSimbolico from "../assets/sociology/capitalSimbolico.png";
 import karl from "../assets/sociology/karl.png"
 
+
+import vaca from '../assets/vaca.png'
+import vacaLeite from '../assets/vacaLeite.png'
+
 const Sociology = () => {
-        return (
-                <>
-                {/* <div>
-                        <h1>Sociologia</h1>
-                        <p>Esta é a página de sociologia.</p>
-                </div> */}
+    return (
+        <div className="soc-container"> 
+
+            <h1 className="soc-firstTitle">JOGO DA MEMÓRIA</h1>
+               
+            <header className="soc-header">
+                Sociologia
+            </header>
                 
-                <h1 className="text-3xl w-64 text-center">Testes</h1>
-          
-                <div className="grid grid-cols-4 gap-4 p-4 ">
-                        <Card content={capitalFinanceiro} />
-                        <Card content={capitalFinanceiro2} />
-                        <Card content={capitalCultural} />
-                        <Card content={capitalSimbolico} />
-                        <Card content={karl} />
-                        <Card content={capitalFinanceiro} />
-                        <Card content={capitalFinanceiro} />
-                        <Card content={capitalFinanceiro} />
-                </div>
+            <div className="grid grid-cols-4 gap-4 p-4">
+                <Card content={capitalFinanceiro} />
+                <Card content={capitalFinanceiro2} />
+                <Card content={capitalCultural} />
+                <Card content={capitalSimbolico} />
+                <Card content={karl} />
+                <Card content={capitalFinanceiro} />
+                <Card content={capitalFinanceiro} />
+                <Card content={capitalFinanceiro} />
+            </div>
                 
-                </>
-        );
+            <div className="soc-buttonNext">
+
+                <button id="shuffle-button">Embaralhe para um novo jogo!</button>
+
+            </div>
+
+            <div>
+
+                <img src={vaca} alt="vaquinha" className="vaca" />
+                <img src={vacaLeite} alt="vaquinha com leite" className="vaca" />
+            
+            </div>
+        </div>
+
+    );
 };
 
 export default Sociology;
