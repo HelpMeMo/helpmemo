84d4bf56677fc5a6479b68cb722b2e2446571ec5
Sophia Moura <araujomsophia@gmail.com>
2025-01-21 12:17:56 -0300

    atu hst e soc

--- src/pages/Sociology.jsx
+++ src/pages/Sociology.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Card from '../components/Card';
 import '../styles/Sociology.css';
+import '../styles/Popup.css';
 
 import capitalFinanceiro from "../assets/sociology/capitalFinanceiro.png";
 import capitalFinanceiro2 from "../assets/sociology/capitalFinanceiro2.png";
@@ -22,51 +23,50 @@ import estado2 from "../assets/sociology/estado2.png";
 
 import vaca from '../assets/vaca.png';
 import vacaLeite from '../assets/vacaLeite.png';
+import vacaFeliz from '../assets/vacaFeliz.png';
 
 const Sociology = () => {
   const [cards, setCards] = useState([
-        { id: 1, content: capitalFinanceiro, pairId: 'pair1', matched: false },
-        { id: 2, content: capitalFinanceiro2,  pairId: 'pair1', matched: false },
-        { id: 3, content: capitalCultural,  pairId: 'pair2', matched: false },
-        { id: 4, content: capitalCultural2,  pairId: 'pair2', matched: false },
-        { id: 5, content: capitalSimbolico,  pairId: 'pair3', matched: false },
-        { id: 6, content: capitalSimbolico2,  pairId: 'pair3', matched: false },
-        { id: 7, content: capitalSocial,  pairId: 'pair4', matched: false },
-        { id: 8, content: capitalSocial2,  pairId: 'pair4', matched: false },
-        { id: 9, content: karlMarx,  pairId: 'pair5', matched: false },
-        { id: 10, content: karlMarx2,  pairId: 'pair5', matched: false },
-        { id: 11, content: emileDurkhein,  pairId: 'pair6', matched: false },
-        { id: 12, content: emileDurkhein2,  pairId: 'pair6', matched: false },
-        { id: 13, content: maxWeber,  pairId: 'pair7', matched: false },
-        { id: 14, content: maxWeber2,  pairId: 'pair7', matched: false },
-        { id: 15, content: estado,  pairId: 'pair8', matched: false },
-        { id: 16, content: estado2,  pairId: 'pair8', matched: false },
-        ]);
-  
-            
+    { id: 1, content: capitalFinanceiro, pairId: 'pair1', matched: false },
+    { id: 2, content: capitalFinanceiro2, pairId: 'pair1', matched: false },
+    { id: 3, content: capitalCultural, pairId: 'pair2', matched: false },
+    { id: 4, content: capitalCultural2, pairId: 'pair2', matched: false },
+    { id: 5, content: capitalSimbolico, pairId: 'pair3', matched: false },
+    { id: 6, content: capitalSimbolico2, pairId: 'pair3', matched: false },
+    { id: 7, content: capitalSocial, pairId: 'pair4', matched: false },
+    { id: 8, content: capitalSocial2, pairId: 'pair4', matched: false },
+    { id: 9, content: karlMarx, pairId: 'pair5', matched: false },
+    { id: 10, content: karlMarx2, pairId: 'pair5', matched: false },
+    { id: 11, content: emileDurkhein, pairId: 'pair6', matched: false },
+    { id: 12, content: emileDurkhein2, pairId: 'pair6', matched: false },
+    { id: 13, content: maxWeber, pairId: 'pair7', matched: false },
+    { id: 14, content: maxWeber2, pairId: 'pair7', matched: false },
+    { id: 15, content: estado, pairId: 'pair8', matched: false },
+    { id: 16, content: estado2, pairId: 'pair8', matched: false },
+  ]);
+
   const [flippedCards, setFlippedCards] = useState([]);
   const [matchedCards, setMatchedCards] = useState([]);
   const [showPopup, setShowPopup] = useState(false);
   const [showNotification, setShowNotification] = useState(false);
   const navigate = useNavigate();
 
-  // Embaralhar os cards
   useEffect(() => {
-        shuffleCards();
-      }, []);
-    
-      const shuffleCards = () => {
-        const shuffled = [...cards].sort(() => Math.random() - 0.5);
-        setCards(shuffled);
-        setFlippedCards([]);
-        setMatchedCards([]);
-        setShowPopup(false);
-      };
+    shuffleCards();
+  }, []);
 
-      const handleCardClick = (id) => {
-        if (flippedCards.length === 2) return;
+  const shuffleCards = () => {
+    const shuffled = [...cards].sort(() => Math.random() - 0.5);
+    setCards(shuffled);
+    setFlippedCards([]);
+    setMatchedCards([]);
+    setShowPopup(false);
+  };
 
-        const clickedCard = cards.find((card) => card.id === id);
+  const handleCardClick = (id) => {
+    if (flippedCards.length === 2) return;
+
+    const clickedCard = cards.find((card) => card.id === id);
     setFlippedCards((prev) => [...prev, clickedCard]);
 
     if (flippedCards.length === 1) {
@@ -80,14 +80,11 @@ const Sociology = () => {
     }
   };
 
-  // Verificar vitória
   useEffect(() => {
-        if (matchedCards.length === cards.length) {
-          setShowPopup(true);
-        }
-      }, [matchedCards, cards]);
-
-      //Codigo da sophi
+    if (matchedCards.length === cards.length) {
+      setShowPopup(true);
+    }
+  }, [matchedCards, cards]);
 
   const handleExitClick = () => {
     setShowNotification(true);
@@ -98,13 +95,12 @@ const Sociology = () => {
   };
 
   const handleConfirmExit = () => {
-    navigate("/"); // Redireciona para a tela inicial
+    navigate("/");
   };
 
   return (
     <div className="soc-container">
       <h1 className="soc-firstTitle">JOGO DA MEMÓRIA</h1>
-
       <header className="soc-header">Sociologia</header>
 
       <div className="grid grid-cols-4 gap-4 p-4">
@@ -132,68 +128,100 @@ const Sociology = () => {
         </div>
 
         {showPopup && (
-                <div className="popup">
-                <h2>Parabéns! Você ganhou!</h2>
-                <button onClick={shuffleCards}>Jogar novamente</button>
-                <button onClick={() => navigate("/")}>Sair</button>
-                </div>
+          <>
+            <div className="overlay"></div>
+            <div className="popup">
+              
+              <h2 style={{ fontSize: "2rem", color: "#4caf50", textShadow: "2px 2px 4px rgba(0, 0, 0, 0.3)"  }}>
+               Parabéns! Você ganhou!
+              </h2>
+              
+              <img src={vacaFeliz} alt="imagem de aviso" 
+              style={{ width: "150px",marginBottom: "20px",display: "block",margin: "0 auto"}}
+          />
+
+
+              <button className="jogarNovamente" 
+              
+              onClick={() => {
+                 shuffleCards(); // Embaralha os cards para um novo jogo
+                 // setShowPopup(false); // Fecha o popup
+                 }}             
+                 > Jogar novamente
+                 
+              </button>
+         
+         
+              <button className="Sair" onClick={() => navigate("/")}
+                >
+                  Sair
+              </button>
+        
+        </div>
+
+          </>
         )}
 
-      </div>
 
+{showNotification && (
+  <>
+    {/* Overlay para congelar a tela */}
+    <div className="telCongelada"></div>
 
-      {showNotification && (
-        <div
+    {/* Notificação */}
+    <div className="notificacao">
+      
+      <h2 className="vaiDesistir" >
+        Vai desistir?
+      </h2>
+
+      <img
+        src={vaca}
+        alt="imagem de aviso"
+        style={{
+          width: "150px",
+          marginBottom: "20px",
+          display: "block",
+          margin: "0 auto",
+        }}
+      />
+      <div>
+
+        <button 
+          onClick={handleCloseNotification}
           style={{
-            position: "fixed",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            backgroundColor: "white",
-            padding: "20px",
-            boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
-            borderRadius: "10px",
-            textAlign: "center",
-            zIndex: 1000,
+            backgroundColor: "#f44336",
+            color: "white",
+            border: "none",
+            padding: "10px 20px",
+            borderRadius: "5px",
+            cursor: "pointer",
+            marginRight: "10px",
+            boxShadow: "0 4px 6px rgba(0, 0, 0, 0.2)",
           }}
         >
-          <h2>Vai desistir?</h2>
-          <img
-            src={vaca}
-            alt="imagem de aviso"
-            style={{ width: "100px", marginBottom: "10px" }}
-          />
-          <div>
-            <button
-              onClick={handleCloseNotification}
-              style={{
-                backgroundColor: "#f44336",
-                color: "white",
-                border: "none",
-                padding: "10px 20px",
-                borderRadius: "5px",
-                cursor: "pointer",
-                marginRight: "10px",
-              }}
-            >
-              Não
-            </button>
-            <button
-              onClick={handleConfirmExit}
-              style={{
-                backgroundColor: "#4caf50",
-                color: "white",
-                border: "none",
-                padding: "10px 20px",
-                borderRadius: "5px",
-                cursor: "pointer",
-              }}
-            >
-              Sim
-            </button>
-          </div>
-        </div>
-      )}
+          Não
+        </button>
+        <button
+          onClick={handleConfirmExit}
+          style={{
+            backgroundColor: "#4caf50",
+            color: "white",
+            border: "none",
+            padding: "10px 20px",
+            borderRadius: "5px",
+            cursor: "pointer",
+            boxShadow: "0 4px 6px rgba(0, 0, 0, 0.2)",
+          }}
+        >
+          Sim
+        </button>
+      </div>
+    </div>
+  </>
+)}
+
+      </div>
 
       <div>
         <img src={vaca} alt="vaquinha" className="vaca" />
