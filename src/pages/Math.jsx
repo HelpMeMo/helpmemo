3e1d34f3899016aec1edccbfe23a7287969894cb
nicolasnk11 <nicolas.nascimento08@aluno.ifce.edu.br>
2025-01-23 12:48:12 -0300

    tentando resolver popup

--- src/pages/Math.jsx
+++ src/pages/Math.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import FrequencyTableGame from '../components/FrequencyTableGame';
 import '../styles/Math.css';
+import vaca from "../assets/vaca.png";
+import vacaLeite from "../assets/vacaLeite.png";
 
 const Math = () => {
         return (
                 <div className="page-math">
-                        <h1 id="titulo-math">Duvido você acertar os valores que faltam na tabela: </h1>
+                                         <img src={vaca} alt="vaquinha" className="vaca" />
+                                         <img src={vacaLeite} alt="vaquinha com leite" className="vaca" />
+                                         <h1 className="mat-firstTitle">TABELA DE FREQUÊNCIA</h1>
+                                         <header className="mat-header">Matematica</header>
                         
                         <FrequencyTableGame />
                 </div>
+                 
         );
+        
 };
 
 export default Math;
